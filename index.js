f91e370354ceebcc84a03516ac5c1ead2fc0431f
giovanni <giovanni.lela@link-me.it>
2018-08-05 17:34:40 +0200

    better api, allow user to directly the access row objects stream

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const objectChunker = require('object-chunker')
 const debug = require('debug')('salmon')
 
 const isEmpty = val => val === undefined || val === null || val === ''
-
 /**
  * Processes a stream containing an XLSX file.
  * Calls the provided async `processor` function.
@@ -22,14 +21,26 @@ module.exports = ({
   limit = Infinity,
   formatting = true,
   chunkSize = 1
-}) =>
-  new Promise((resolve, reject) => {
-    let i = 0
-    let cols = null
-    let detected = null
+}) => {
+  let cols = null
+  let detected = false
+  let stream
+  let detector
+  let reader
+  const onErr = err => {
+    if (!err) return
+    debug(err)
+    inputStream.destroy()
+    detector.destroy()
+    reader.removeAllListeners('row')
+    stream.destroy(err)
+  }
 
-    const toStream = reader => {
-      const s = new Transform({
+  return {
+    stream () {
+      detector = new FileType()
+
+      stream = new Transform({
         objectMode: true,
         transform (chunk, enc, cb) {
           if (chunk.every(isEmpty)) { return cb() }
@@ -41,94 +52,98 @@ module.exports = ({
           cb()
         }
       })
-      s.on('drain', () => {
-        debug('resume stream')
-        reader.workSheetStream.resume()
-      })
-      reader.on('row', function onRow (row, i) {
-        debug('row received')
-        try {
-          if (!cols) {
-            if (reader.sheetData.dimension) {
-              const lines = reader.sheetData.dimension[0].attributes.ref.match(/\d+$/)
-              onLineCount(parseInt(lines, 10) - 1)
-            }
-            cols = row.values.map(mapColumns)
-          } else if (!s.write(row.values)) {
-            debug('pausing stream')
-            reader.workSheetStream.pause()
-          }
-        } catch (err) {
-          inputStream.destroy(err)
-          reject(err)
-          reader.removeListener('row', onRow)
-        }
-      })
-      return s
-    }
 
-    const readSheet = (workSheetReader) => {
-      detected = true
-      workSheetReader.workSheetStream.on('error', reject)
-      // read only the first worksheet for now
-      if (workSheetReader.id > 1) { workSheetReader.skip(); return }
-      const readStream = toStream(workSheetReader)
-      const processRow = new Writable({
-        objectMode: true,
-        async write (row, encoding, cb) {
-        // if this fails, pump will cleanup the broken streams.
+      const checkAndPipe = (fileType) => {
+        if (!fileType || fileType.mime !== 'application/zip') {
+          onErr(new Error('Invalid file type'))
+        } else {
+          pump(detector, workBookReader, onErr)
+          workBookReader.on('end', () => {
+            if (!detected) {
+              onErr(new Error('Invalid file type'))
+            }
+          })
+        }
+      }
+
+      const readSheet = workSheetReader => {
+        detected = true
+        reader = workSheetReader
+        workSheetReader.workSheetStream.on('error', onErr)
+        // read only the first worksheet for now
+        if (workSheetReader.id > 1) { workSheetReader.skip(); return }
+        // worksheet reader is an event emitter - we have to convert it to a read stream
+        // signal stream end when the event emitter is finished
+        workSheetReader.on('end', () => stream.push(null))
+        workSheetReader.process()
+
+        stream.on('drain', () => {
+          debug('resume stream')
+          workSheetReader.workSheetStream.resume()
+        })
+        workSheetReader.on('row', function onRow (row, i) {
+          debug('row received')
           try {
-            await processor(row, i)
-            i += chunkSize
-            if (i === limit) {
-              readStream.destroy()
-              inputStream.destroy()
-              this.destroy()
-              // a premature close error will be raised.. this could actually be good as pump
-              // will unpipe and cleanup
-              return resolve(i)
+            if (!cols) {
+              if (workSheetReader.sheetData.dimension) {
+                const lines = workSheetReader.sheetData.dimension[0].attributes.ref.match(/\d+$/)
+                onLineCount(parseInt(lines, 10) - 1)
+              }
+              cols = row.values.map(mapColumns)
+            } else if (!stream.write(row.values)) {
+              debug('pausing stream')
+              workSheetReader.workSheetStream.pause()
             }
           } catch (err) {
-            return cb(err)
-          }
-          return cb(null)
-        }
-      })
-      // worksheet reader is an event emitter - we have to convert it to a read stream
-      // signal stream end when the event emitter is finished
-      workSheetReader.on('end', () => readStream.push(null))
-      workSheetReader.process()
-      const pipeline = chunkSize > 1
-        ? [readStream, objectChunker(chunkSize), processRow]
-        : [readStream, processRow]
-
-      pump(pipeline, (err) => err ? reject(err) : resolve(i))
-    }
-
-    const checkAndPipe = (fileType) => {
-      if (!fileType || fileType.mime !== 'application/zip') {
-        inputStream.destroy()
-        detector.destroy()
-        reject(new Error('Invalid file type'))
-      } else {
-        pump(detector, workBookReader, (err) => {
-          if (err) { reject(err) }
-        })
-        workBookReader.on('end', () => {
-          if (!detected) {
-            reject(new Error('Invalid file type'))
+            onErr(err)
           }
         })
       }
+      const workBookReader = new XlsxStreamReader({ formatting })
+      workBookReader.on('error', onErr)
+      workBookReader.on('worksheet', readSheet)
+
+      detector.on('file-type', checkAndPipe)
+      pump(inputStream, detector, onErr)
+
+      return chunkSize > 1 ? pump(stream, objectChunker(chunkSize)) : stream
+    },
+    processor ({ onRow, limit }) {
+      let i = 0
+      const readStream = this.stream()
+      return new Promise((resolve, reject) => {
+        const processRow = new Writable({
+          objectMode: true,
+          async write (row, encoding, cb) {
+            // if this fails, pump will cleanup the broken streams.
+            try {
+              await onRow(row, i)
+              i += chunkSize
+              if (i === limit) {
+                reader.removeAllListeners('row')
+                readStream.destroy()
+                detector.destroy()
+                inputStream.destroy()
+                this.destroy()
+                // a premature close error will be raised.. this could actually be good as pump
+                // will unpipe and cleanup
+                return resolve(i)
+              }
+            } catch (err) {
+              return cb(err)
+            }
+            return cb(null)
+          }
+        })
+        pump(readStream, processRow, err => {
+          onErr(err)
+          if (err) {
+            reject(err)
+          } else {
+            resolve(i)
+          }
+        })
+      })
     }
-
-    const workBookReader = new XlsxStreamReader({ formatting })
-    workBookReader.on('error', reject)
-    workBookReader.on('worksheet', readSheet)
-
-    const detector = new FileType()
-    detector.on('file-type', checkAndPipe)
-    pump(inputStream, detector, (err) => {
-      if (err) { reject(err) }
-    })
-  })
+  }
+}
--- test/salmon.test.js
+++ test/salmon.test.js
@@ -8,12 +8,13 @@ describe('XSLT Processor', () => {
   it('processes an xslt file.', async () => {
     const processed = await XLSXProcessor({
       inputStream: createReadStream(join(__dirname, 'fixtures', 'error.xlsx')),
-      processor: async (data, i) => {
+      mapColumns: colName => colName.toLowerCase().trim()
+    }).processor({
+      onRow: async (data, i) => {
         expect(i).to.be.a('number')
         // console.log('DAAAAATAA', data)
         expect(data['first name']).to.be.ok
-      },
-      mapColumns: colName => colName.toLowerCase().trim()
+      }
     })
     expect(processed).to.equal(19)
   })
@@ -22,24 +23,26 @@ describe('XSLT Processor', () => {
     const processed = await XLSXProcessor({
       chunkSize: 2,
       inputStream: createReadStream(join(__dirname, 'fixtures', 'prova.xlsx')),
-      processor: async (data, i) => {
+      mapColumns: colName => colName.toLowerCase().trim()
+    }).processor({
+      onRow: async (data, i) => {
         expect(data).to.have.lengthOf(2)
         expect(i % 2).to.equal(0)
-      },
-      mapColumns: colName => colName.toLowerCase().trim()
+      }
     })
     expect(processed).to.equal(6)
   })
 
   it('formats by default', async () => {
     const inputStream = createReadStream(join(__dirname, 'fixtures', 'dates.xlsx'))
-    const processed = await XLSXProcessor({
+    await XLSXProcessor({
       inputStream,
-      processor: async (data, i) => {
+      mapColumns: i => i
+    }).processor({
+      onRow: async (data, i) => {
         expect(data['Data di nascita']).to.match(/\d{2}\/\d{2}\/\d{4}/)
         expect(data['cap']).to.match(/\d{5}/)
-      },
-      mapColumns: i => i
+      }
     })
   })
 
@@ -47,23 +50,25 @@ describe('XSLT Processor', () => {
     const inputStream = createReadStream(join(__dirname, 'fixtures', 'predefined_formats.xlsx'))
     const processed = await XLSXProcessor({
       inputStream,
-      processor: async (data, i) => {
+      mapColumns: i => i
+    }).processor({
+      onRow: async (data, i) => {
         expect(data['Data di nascita']).to.match(/\d\/\d{1,2}\/\d{2}/)
         expect(data['cap']).to.match(/\d{5}/)
-      },
-      mapColumns: i => i
+      }
     })
   })
 
   it('can disable formatting', async () => {
     const inputStream = createReadStream(join(__dirname, 'fixtures', 'dates.xlsx'))
-    const processed = await XLSXProcessor({
+    await XLSXProcessor({
       inputStream,
-      processor: async (data, i) => {
-        expect(data['Data di nascita']).to.match(/^\d{5}$/)
-      },
       mapColumns: i => i,
       formatting: false
+    }).processor({
+      onRow: async (data, i) => {
+        expect(data['Data di nascita']).to.match(/^\d{5}$/)
+      }
     })
   })
 
@@ -71,9 +76,10 @@ describe('XSLT Processor', () => {
     const inputStream = createReadStream(join(__dirname, 'fixtures', 'test.xlsx'))
     const processed = await XLSXProcessor({
       inputStream,
-      processor: async (data, i) => {
+      mapColumns: colName => colName.toLowerCase().trim()
+    }).processor({
+      onRow: async (data, i) => {
       },
-      mapColumns: colName => colName.toLowerCase().trim(),
       limit: 10
     })
     expect(processed).to.equal(10)
@@ -82,9 +88,11 @@ describe('XSLT Processor', () => {
   it('processes largeish files', async () => {
     await XLSXProcessor({
       inputStream: createReadStream(join(__dirname, 'fixtures', 'big.xlsx')),
-      processor: async () => {
-      },
       mapColumns: colName => colName.toLowerCase().trim()
+    }).processor({
+      onRow: async () => {
+       await Promise.delay(500)
+      }
     })
   })
 
@@ -92,8 +100,9 @@ describe('XSLT Processor', () => {
     try {
       await XLSXProcessor({
         inputStream: createReadStream(join(__dirname, 'fixtures', 'broken.xlsx')),
-        processor: async () => {},
         mapColumns: colName => colName.toLowerCase().trim()
+      }).processor({
+        onRow: async () => {}
       })
     } catch (e) {
       expect(e.message).to.equal('Unknown column ID')
